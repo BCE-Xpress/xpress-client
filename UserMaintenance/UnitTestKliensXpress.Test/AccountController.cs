5938991b8bbb6ef4311e02d70f53943ea8470726
Kristóf Bíró <kristof.biro@stud.uni-corvinus.hu>
2023-05-01 19:14:16 +0200

    Unit tesztek

---
+++ UserMaintenance/UnitTestKliensXpress.Test/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UnitTestKliensXpress
+{
+    public class AccountController
+    {
+        
+        public bool ValidateQuantity(int mennyiseg)
+        {
+
+
+            //A mennyiseg nem lehet nagyobb, mint 200
+            if (mennyiseg > 200)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+
+        public bool ValidatePassword(string password)
+        {
+
+            // Vizsgáljuk, hogy helyes-e a jelszo ami jelen esetben 0
+            if (password == "0")
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ UserMaintenance/UnitTestKliensXpress.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestKliensXpress.Test
+{
+    internal class AccountControllerTestFixture
+    {
+        [
+       Test,
+       TestCase("abcd1234", false),
+       TestCase("a23", false),
+       TestCase("2", false),
+       TestCase("0", true)
+       ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+
+            // Arrange
+            var accountController = new AccountController();
+
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+
+        [
+       Test,
+       TestCase("abcd1234", false),
+       TestCase("a23", false),
+       TestCase("250", false),
+       TestCase("22", true)
+       ]
+        public void TestValidateQuantity(int quantity, bool expectedResult)
+        {
+
+            // Arrange
+            var accountController = new AccountController();
+
+
+            // Act
+            var actualResult = accountController.ValidateQuantity(quantity);
+
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+    }
+}
