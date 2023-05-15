431e712893a25924988dc4eb3594bfe573eaaf1b
Kristóf Bíró <kristof.biro@stud.uni-corvinus.hu>
2023-05-15 21:59:25 +0200

    Unit test probalkozas

---
+++ UserMaintenance/KliensTest/UserManagementTesting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using KliensTest.Tesztek;
+
+namespace KliensTest
+{
+    public class UserManagementTesting
+    {
+        [Fact]
+        public void Add_CreateUser()
+        {
+
+            //Arrange
+            var userManagement = new UserManagement();
+
+
+            //Act
+            userManagement.Add(new("Andris"));
+
+            //Assert
+            var savedUser = Assert.Single(userManagement.AllUsers);
+            Assert.NotNull(savedUser); // Ellenorzes, hogy a usermanagement list nem ures
+            Assert.Equal("Andris", savedUser.Firstname);
+            Assert.False(savedUser.VerifiedName);
+        }
+
+    }
+}
---
+++ UserMaintenance/UserMaintenance/Tesztek/UserManagement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public record User(string Firstname, string Lastname)
+{
+    public int Id { get; set; }
+
+    public int Name { get; set; }
+
+    public string Jelszo { get; set; }
+
+    public bool VerifiedName { get; set; }
+
+}
+
+namespace KliensTest.Tesztek
+{
+    public class UserManagement
+    {
+        private readonly List<User> _users = new List<User>();
+
+        private int idCounter = 1;
+
+        public IEnumerable<User> AllUsers => _users;
+        
+
+        public void Add(User user)
+        {
+            _users.Add(user with {Id = idCounter++});
+        }
+
+        public void VerifyName(int userID)
+        {
+            var dbUser = _users.First(x => x.Id == userID);
+
+            dbUser.VerifiedName = true;
+        }
+
+    }
+}
